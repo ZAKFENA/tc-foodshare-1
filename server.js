5246d92461b996c2cf8759162d7d470a48582516
Zekarias Negassa <zakfena@gmail.com>
2020-12-23 22:25:25 -0600

    added app.get in server.js for directing routes to index.html

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+};
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // Add routes, both API and view
 app.use("/api/", foodItemRoutes);
